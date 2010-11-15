4b9088aaef3313883828ca656f37df5725c25143
Christopher Finke <cfinke@gmail.com>
2010-11-15 02:39:25 +0000

    Removing .git files when building

--- build-xpi.sh
+++ build-xpi.sh
@@ -4,7 +4,7 @@ rm -f rss-ticker.xpi
 mkdir .xpi_work_dir
 cp -r rss-ticker/* .xpi_work_dir/
 cd .xpi_work_dir/
-rm -rf `find . -name ".svn"`
+rm -rf `find . -name ".git"`
 rm -rf `find . -name ".DS_Store"`
 rm -rf `find . -name "Thumbs.db"`
 zip -rq ~/Desktop/rss-ticker.xpi *
