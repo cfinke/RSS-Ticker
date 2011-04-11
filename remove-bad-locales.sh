7ecbff898096baaa5851bec7c55259a8529bc79d
Christopher Finke <cfinke@gmail.com>
2011-04-11 23:20:09 +0000

    Ignoring pl locale

--- remove-bad-locales.sh
+++ remove-bad-locales.sh
@@ -1,3 +1,3 @@
 git checkout rss-ticker/chrome/locale/en-US/
-rm -rf rss-ticker/chrome/locale/{zh-CN,de-DE,es-AR,fr-FR,nl-NL,uk-UA,pt-PT}
+rm -rf rss-ticker/chrome/locale/{zh-CN,de-DE,es-AR,fr-FR,nl-NL,uk-UA,pt-PT,pl}
 rm -rf `find . -name "contents.rdf"`
