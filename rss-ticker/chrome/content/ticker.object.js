ee9ceabd1affdf030450014344328aaaf35f5c6c
Christopher Finke <cfinke@gmail.com>
2011-08-25 04:28:28 +0000

    Fixing data URI for stylesheet to use encodeURIComponent now that Firefox 6 supports #hashes in data URIs

--- rss-ticker/chrome/content/ticker.object.js
+++ rss-ticker/chrome/content/ticker.object.js
@@ -739,7 +739,7 @@ var RSSTICKER = {
 					}
 				}
 				
-				var data = 'data:text/css;charset=utf-8,' + encodeURI(css);
+				var data = 'data:text/css;charset=utf-8,' + encodeURIComponent(css);
 				var sss = Components.classes["@mozilla.org/content/style-sheet-service;1"].getService(Components.interfaces.nsIStyleSheetService);
 				var ios = Components.classes["@mozilla.org/network/io-service;1"].getService(Components.interfaces.nsIIOService);
 				
